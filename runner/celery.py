857d563d6ee61d1cd1a1f858317d4764276046ee
dbpython <33459750+papismurf@users.noreply.github.com>
2024-05-15 11:36:02 +0100

    Added broker connection retry on startup in Celery configuration. This ensures that the worker will attempt to reconnect to the broker if the initial connection is unsuccessful during startup.

--- runner/celery.py
+++ runner/celery.py
@@ -2,8 +2,17 @@ import os
 
 from celery import Celery
 
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'runner.settings')
 
 app = Celery('runner')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.conf.broker_connection_retry_on_startup = True
+
+# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
